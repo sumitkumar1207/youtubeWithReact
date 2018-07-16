44ec4a8b82785168b0753cc5a75c26e3f7c4f6a1
sumitkumar <sumit@binarynumbers.io>
2018-07-16 12:55:34 +0530

    ''

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+import API_KEY from '../config/keys';
+
+//Create a new component. This component should produce some html
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("rahul gandhi pappu");
+  }
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+  render() {
+    const videoSearch = _.debounce((term)=> {this.videoSearch(term)},500);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}/>
+        {/* <VideoListItems /> */}
+      </div>
+    );}
+}
+
+//Take this component's genrated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
